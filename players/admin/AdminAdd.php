e0e7f09a1b5176328298df82a27286802c77c5fe
21musie <104737346+21musie@users.noreply.github.com>
2025-01-23 23:55:24 +0300

    Team Member Addition Form
    decorated with comments

--- players/admin/AdminAdd.php
+++ players/admin/AdminAdd.php
@@ -4,59 +4,84 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>adding data</title>
+    <title>Adding Data</title>
     <link rel="stylesheet" href="adminStyle.css">
 </head>
 <body>
     <div class="wrapper">
-    <form action="AdminAdd.php" method="post" enctype="multipart/form-data">
-        <h3>first name</h3><input class="fname" pattern="[A-Za-z]+" required type="text" name="fname" ><br>
-        <h3>last name </h3><input class="lname" pattern="[A-Za-z]+" required type="text" name="lname" ><br>
-        <h3>age </h3><input class="age" required type="number" name="age" min="17" max="50" ><br>
-        <h3>description </h3><textarea class="description" name="about"  cols="30" rows="10"></textarea><br>
-        <h3>image</h3><input class="images" required type="file" name="image" ><br>
-         <h3>status</h3><select  name="status" >
-        <option value="goalkeeper">GOAl KEEPER</option>
-        <option value="defender" >DEFENDER</option>
-        <option value="midfilder">MIDFILDER</option>
-        <option value="forward">FORWARD</option>
-    </select><br>
+        <!-- Form for adding new team members -->
+        <form action="AdminAdd.php" method="post" enctype="multipart/form-data">
+            <h3>First Name</h3>
+            <input class="fname" pattern="[A-Za-z]+" required type="text" name="fname"><br>
+            
+            <h3>Last Name</h3>
+            <input class="lname" pattern="[A-Za-z]+" required type="text" name="lname"><br>
+            
+            <h3>Age</h3>
+            <input class="age" required type="number" name="age" min="17" max="50"><br>
+            
+            <h3>Description</h3>
+            <textarea class="description" name="about" cols="30" rows="10"></textarea><br>
+            
+            <h3>Image</h3>
+            <input class="images" required type="file" name="image"><br>
+            
+            <h3>Status</h3>
+            <select name="status">
+                <option value="goalkeeper">GOAL KEEPER</option>
+                <option value="defender">DEFENDER</option>
+                <option value="midfilder">MIDFIELDER</option>
+                <option value="forward">FORWARD</option>
+            </select><br>
 
-        <input class="submit" type="submit" name=" submit" value="send">
-    </form>
+            <input class="submit" type="submit" name="submit" value="Send">
+        </form>
     </div>
 </body>
 </html>
 
-
 <?php
+// Include database connection file
 include('../../connect.php');
+
+// Instantiate connection class
 $conn = new connect;
 $connection = $conn->getConn();
-if(!$conn){
-    die('error'.mysqli_connect_error());
+
+// Check for a successful connection
+if(!$conn) {
+    die('error' . mysqli_connect_error());
 }
- if(isset($_POST['submit'])){
-    $image=$_FILES['image']['name'];
-    $fname=$_POST['fname']; 
-    $lname=$_POST['lname'];
-    $age=$_POST['age'];
-    $description=$_POST['about'];
-    $stat=$_POST['status'];
 
-    $query="INSERT into team values('','$fname','$lname','$image','$description','$age','$stat')";
-    if($query==true){
-        move_uploaded_file($_FILES['image']['tmp_name'],"../images/$image");
-        echo' <script>alert("successfully added")</script>';
-        //header('location:../admin');
-    }
-    $result=$connection->query($query);
-    if(!$result){
-        die('error'.mysqli_connect_error());
-    }else{
-        //echo 'data sucessfully added';
+// Check if the form is submitted
+if(isset($_POST['submit'])) {
+    // Collect form data and file upload
+    $image = $_FILES['image']['name'];
+    $fname = $_POST['fname']; 
+    $lname = $_POST['lname'];
+    $age = $_POST['age'];
+    $description = $_POST['about'];
+    $stat = $_POST['status'];
+
+    // Prepare SQL query to insert data into the team table
+    $query = "INSERT INTO team VALUES('', '$fname', '$lname', '$image', '$description', '$age', '$stat')";
+    
+    // Check if the query is well-formed (not true, but checking if it runs)
+    if($query) {
+        // Move uploaded file to the specified directory
+        move_uploaded_file($_FILES['image']['tmp_name'], "../images/$image");
+        echo '<script>alert("Successfully added")</script>';
+        // Optionally redirect to admin page after successful addition
+        // header('location:../admin');
     }
 
- }
+    // Execute the query and check for errors
+    $result = $connection->query($query);
+    if(!$result) {
+        die('error' . mysqli_connect_error());
+    } else {
+        // Optionally confirm successful data addition
+        // echo 'Data successfully added';
+    }
+}
 ?>
-
