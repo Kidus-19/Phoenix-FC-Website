e390ed9d431e0731a55d9bea28413d35e67b8a7d
Kidus-19 <105101940+Kidus-19@users.noreply.github.com>
2025-02-09 23:05:43 +0300

    Adding Bootstrap

--- fixtures/fixtures.php
+++ fixtures/fixtures.php
@@ -1,8 +1,8 @@
 <?php
-    include ('../connect.php');
-    include ('../common/adminMenu.php');
-    $conn = new connect;
-    $conn = $conn->getConn();
+include ('../connect.php');
+include ('../common/adminMenu.php');
+$conn = new connect;
+$conn = $conn->getConn();
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -12,73 +12,57 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Fixtures</title>
     <link rel="shortcut icon" href="img/logo3.png" type="image/x-icon">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="style/fixtures.css">
 </head>
-<body >
-    <div class="wrapper" >
-
-                <?php
-                 $result = mysqli_query($conn,"Select * from fixtures order by date desc");
-                 $latest = $result->fetch_assoc();
-                echo "<main class='latestMatch'>
-                            <section>
-                        <figure class='home Team'>";
-                 echo "<img src='img/Phoenix.png' alt='Team Logo' class='logo'>";
-                 echo "<figcaption><h2>Phoenix</h2></figcaption>";
-                 echo "</figure>";
-                //  $ticket = mysqli_query($conn,"select id from fixtures where name ='Phoenix vs ".$latest['Name']." on ".$latest['DATE']."'")->fetch_assoc();
-                 echo "<p class='vs'>
-                            Vs
-                        </p>                 
+<body>
+    <div class="wrapper container mt-5">
+        <?php
+        $result = mysqli_query($conn, "SELECT * FROM fixtures ORDER BY date DESC");
+        $latest = $result->fetch_assoc();
+        echo "<main class='latestMatch mb-4'>
+                <section class='text-center'>
+                    <figure class='home Team'>
+                        <img src='img/Phoenix.png' alt='Team Logo' class='logo img-fluid'>
+                        <figcaption><h2>Phoenix</h2></figcaption>
+                    </figure>
+                    <p class='vs'>Vs</p>
+                    <figure class='opponent'>
+                        <img src='img/".$latest['Name'].".png' alt='' class='logo img-fluid'>
+                        <figcaption><h2>".$latest['Name']."</h2></figcaption>
+                    </figure>
+                </section>
+                <p class='detail date'>".$latest['DATE']."</p>
+                <p class='detail location'>".$latest['location']."</p>
+                <a href='buyTickets.php?id=".$latest['id']."'><button id='buy' class='btn btn-primary'>Buy Ticket Now</button></a>
+              </main>";  
+        ?>
+        <aside class="future">
+            <?php
+            while ($other = $result->fetch_assoc()) {
+                echo "<main class='mb-4'>
+                        <section class='text-center'>
+                            <figure class='home Team'>
+                                <img src='img/Phoenix.png' alt='Team Logo' class='logo img-fluid'>
+                                <figcaption><h2>Phoenix</h2></figcaption>
+                            </figure>
+                            <p class='vs'>Vs</p>
                             <figure class='opponent'>
-                                <img src='img/".$latest['Name'].".png' alt='' class='logo'>
-                                <figcaption>
-                                    <h2>".$latest['Name']."</h2>
-                                </figcaption>
+                                <img src='img/".$other['Name'].".png' alt='Team Logo' class='logo img-fluid'>
+                                <figcaption><h2>".$other['Name']."</h2></figcaption>
                             </figure>
                         </section>
-                        
-                            <p class='detail date'>".$latest['DATE']."</p>
-                            <p class='detail location'>".$latest['location']."</p>
-                            <a href='buyTickets.php?id=".$latest['id']."'><button id='buy' >Buy Ticket Now</button></a>
-                    
-                        </main>";  
-                ?>
-        <aside class="future">
-                <?php
-                    while($other=$result->fetch_assoc()){
-                        echo "<main> <section>
-                        <figure class='home Team'>
-                            <img src='img/Phoenix.png' alt='Team Logo' class='logo'>
-                            <figcaption>
-                                <h2>Phoenix</h2>
-                            </figcaption>
-                            </figure>
-                                <p class='vs'>
-                                    Vs
-                                </p>
-                        
-                            <figure class='opponent'>
-                            <img src='img/".$other['Name'].".png' alt='Team Logo' class='logo'>
-                                <figcaption>
-                                    <h2>".$other['Name']."</h2>
-                                </figcaption>
-                            </figure>
-                            
-                            </section>
-                            <p class='detail date'>".$other['DATE']."</p>
-                            <p class='detail location'>".$other['location']."</p>
-                            <a href='buyTickets.php?id=".$other['id']."'><button id='buy' >Buy Ticket Now</button></a>
-                    
-                            </main>";
-                    } 
-                ?>
-                <?php
-                    
-                    ?>
-               
+                        <p class='detail date'>".$other['DATE']."</p>
+                        <p class='detail location'>".$other['location']."</p>
+                        <a href='buyTickets.php?id=".$other['id']."'><button id='buy' class='btn btn-primary'>Buy Ticket Now</button></a>
+                      </main>";
+            } 
+            ?>
         </aside>
     </div>
+    
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js"></script>
+    <script src="script/fixtures.js"></script>
 </body>
 </html>
-<script src="script/fixtures.js"></script>
